367ce264c2cf41c8e91c7623882c006510f02bcc
DariaGavrilenko <gavrilenko10919@gmail.com>
2023-01-08 18:00:56 +0300

    feat:added profile-page

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { AppRootStateType, useAppDispatch, useAppSelector } from '../../app/store';
+import { authMeThunk, changeNameThunk, loginThunk, logOutThunk, registarationThunk } from './profile-slice';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
+import LocalSeeOutlinedIcon from '@mui/icons-material/LocalSeeOutlined';
+import style from './Profile.module.css'
+import logoutIcon from './logout.svg'
 
 export const Profile = () => {
+    const dispatch = useAppDispatch()
+    const isLoggedIn = useAppSelector(state => state.profile.isLoggedIn)
+    const profileData = useAppSelector(state => state.profile.profileData)
+    const [isEditMode, setEditMode] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+    const [userName, setUserName] = useState<string>(profileData.name)
+
+    useEffect(() => {
+        dispatch(authMeThunk())
+    }, [])
+    console.log(isLoggedIn);
+    console.log(profileData);
+    // if(!isLoggedIn){
+    //     return <Navigate to='/login'/>
+    // }
+    const avatar = profileData.avatar ? profileData.avatar : 'https://static.thenounproject.com/png/707608-200.png'
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.currentTarget.value)
+        if (!e.currentTarget.value.trim()) {
+            setError('requered')
+        } else {
+            setError('')
+            setUserName(e.currentTarget.value)
+        }
+    }
+    const onClickHandler = () => {
+        setEditMode(false)
+        dispatch(changeNameThunk(userName))
+    }
     return (
-        <div>
-            Profile page
-            <h1>Profile</h1>
+        <div className={style.profileContainer}>
+            <h1 className={style.profileTitle}>Personal information</h1>
+            <div className={style.profileAvatar}>
+                <img src={avatar} alt="Yours avatar" />
+                <IconButton>
+                <LocalSeeOutlinedIcon fontSize='small' className={style.photoIcon}/>
+                </IconButton>
+            </div>
+            {isEditMode ? <div className={style.profileInputContainer}>
+                <TextField
+                    error={!!error}
+                    id="standard-error-helper-text"
+                    label={error ? 'error' : 'nick name'}
+                    defaultValue={profileData.name}
+                    helperText={error ? error : ''}
+                    variant="standard"
+                    onChange={onChangeHandler}
+                    className={style.inputName}
+                    onBlur={()=>setEditMode(false)}
+                />
+                <button onClick={onClickHandler} disabled={!!error} className={style.saveButton}>SAVE</button>
+            </div> : 
+            <div className={style.profileNameContainer} >
+            <div onDoubleClick={() => setEditMode(true)} className={style.profileName}>{profileData.name}</div>
+            <IconButton >
+                <BorderColorOutlinedIcon fontSize='small' onClick={() => setEditMode(true)} />
+            </IconButton>
+        </div>}
+            <div className={style.profileEmail}>{profileData.email}</div>
+            <button onClick={() => { dispatch(logOutThunk()) }} className={style.logOutButton}>
+                <img src={logoutIcon} alt="" />
+                Log out
+                </button>
+            {/* <button onClick={() => { dispatch(registarationThunk()) }} >Registartion</button>
+            <button onClick={() => { dispatch(loginThunk()) }} >Login</button> */}
+            
+            
+
         </div>
     );
 };
--- src/features/Profile/profile-slice.ts
+++ src/features/Profile/profile-slice.ts
@@ -1,33 +1,119 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {AnyAction, createSlice, Dispatch} from "@reduxjs/toolkit";
 import {AppDispatch} from "../../app/store";
+import { profileApi } from "./profileApi";
 
-
+export type ProfileDataType = {
+    _id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+    created: Date
+    updated: Date
+    isAdmin: boolean
+    verified: boolean
+    rememberMe: boolean
+}
+export type initialStateType = {
+    isLoggedIn: boolean
+    profileData: ProfileDataType
+}
 const initialState = {
+isLoggedIn: false,
+profileData:{
+    _id: '',
+    email: '',
+    name: '',
+    avatar: '',
+    publicCardPacksCount: 0, // количество колод
+    created: Date,
+    updated: Date,
+    isAdmin: false,
+    verified: false, // подтвердил ли почту
+    rememberMe: false,
+}
 }
 
 const slice = createSlice({
     name: 'profile',
     initialState: initialState,
-    reducers: {}
+    reducers: {
+        setIsLoggedIn(state, action){
+            state.isLoggedIn = action.payload.status
+        },
+        setProfileData(state,action){
+            state.profileData = {...action.payload.data}
+        },
+        setNewName(state,action){
+            state.profileData.name = action.payload.name
+        }
+    }
 
 })
 
 export const profileSlice = slice.reducer
 
 
+export const {setIsLoggedIn,setProfileData,setNewName} = slice.actions
 
-
-export const thunk = () => async (dispatch: any) => {
-
+export const authMeThunk = () => async (dispatch: Dispatch<AnyAction>) => {
     try {
-
+        let res = await profileApi.me()
+        dispatch(setIsLoggedIn({status:true}))
+        dispatch(setProfileData({data:res.data}))
+        console.log(res.data)
     } catch (e) {
-
-
+        console.log(e)
     } finally {
 
     }
 }
+
+export const registarationThunk = () => async (dispatch: Dispatch<AnyAction>) => {
+    try {
+        let res = await profileApi.registration()
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    } finally {
+
+    }
+}
+
+export const loginThunk = () => async (dispatch: Dispatch<AnyAction>) => {
+    try {
+        let res = await profileApi.login()
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    } finally {
+
+    }
+}
+
+export const logOutThunk = () => async (dispatch: Dispatch<AnyAction>) => {
+    try {
+        let res = await profileApi.logOut()
+        dispatch(setIsLoggedIn({status:false}))
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    } finally {
+
+    }
+}
+export const changeNameThunk = (name:string) => async (dispatch: Dispatch<AnyAction>) => {
+    try {
+        let res = await profileApi.changeProfileData(name)
+        dispatch(setNewName({name:name}))
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    } finally {
+
+    }
+}
+
 
 
 export type AppActionsType = any
