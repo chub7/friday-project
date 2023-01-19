f269a4ab23256603a31a61dbd69fbec5114d4dbe
DariaGavrilenko <gavrilenko10919@gmail.com>
2023-01-19 15:09:59 +0300

    fix: fix card table bugs

--- src/features/studies-page/cards-list/empty-pack-cards/empty-pack.tsx
+++ src/features/studies-page/cards-list/empty-pack-cards/empty-pack.tsx
@@ -1,33 +1,34 @@
 import React, {FC} from 'react';
 import {GeneralButton} from "../../../../utils/StyleForMUI/StyleForMUI";
 import style from "./empty-pack.module.css"
-import {useAppDispatch} from "../../../../app/store";
+import {useAppDispatch, useAppSelector} from "../../../../app/store";
 import {useParams} from 'react-router-dom';
 import {addNewCard, setCardsError, setSuccessStatusForSnackBar} from '../cards-slice';
 import {CardModel} from '../CardModel';
 import {BackToPackLink} from "../../../../components/BackLink/BackToPackLink";
 import styles from "../../studies-page.module.css";
 import {UniversalSnackbar} from "../../../../components/SnackBar/Snackbar";
+import { cardErrorSelector, successStatusForSnackBarSelector } from '../cards-selectors';
 type EmptyPackType={
     namePack:string
-    error:string|null
-    success:string
-    open:boolean
-
 
 }
-export const EmptyPack:FC<EmptyPackType> = ({namePack,error,success,open}) => {
+export const EmptyPack:FC<EmptyPackType> = ({namePack}) => {
 
     const dispatch = useAppDispatch()
     const params = useParams()
     const model = CardModel()
+  const success = useAppSelector(successStatusForSnackBarSelector)
+    const error = useAppSelector(cardErrorSelector)
 
     const onClickHandler = () => {
         dispatch(addNewCard(params.id))
     }
 
+  const open = error !== null || !!success
+
     return (
-        <>
+        <div className={styles.wholeForm}>
             <BackToPackLink/>
             <h3 className={styles.ownerName}>{namePack}</h3>
             {model.myPack ?
@@ -43,7 +44,7 @@ export const EmptyPack:FC<EmptyPackType> = ({namePack,error,success,open}) => {
             {open && <UniversalSnackbar error={error} changeError={setCardsError} success={success}
                                         changeSuccess={setSuccessStatusForSnackBar}/>}
 
-        </>
+        </div>
     );
 };
 
